63204aa74eaffe9517163aeed1b0aa3033474640
Terence961110 <112816590+Terence961110@users.noreply.github.com>
2022-09-04 23:35:14 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ export const config = {
    */
 
   // 公众号APP_ID
-  APP_ID: "",
+  APP_ID: "wxaeee51261d76dcce",
 
   // 公众号APP_SECRET
-  APP_SECRET: "",
+  APP_SECRET: "8329ede33185ee8a646ba8ea66ddc990",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
@@ -24,20 +24,20 @@ export const config = {
    *   }, 
    * ]
    */
-  USERS: [
+  USERS: ["li_wei_1110"]
     {
       // 想要发送的人的名字
-      name: "老婆0",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      name: "臭猪",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到,"sxt1414442825"
+      id: "oDXlU6ItPg9HkwhBVntbvteFbLLQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "fbTN6qpTeQAWJP3j_VrmxIsBv3-jgkRev5kJpKrullo",
       // 所在省份
-      province: "广东",
+      province: "江苏",
       // 所在城市
-      city: "肇庆",
+      city: "常州",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://y.music.163.com/m/playlist?id=166723731&userid=132514428&creatorId=132514428"
     }, 
     {
       name: "老婆1", 
@@ -70,7 +70,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+  CALLBACK_TEMPLATE_ID: "fbTN6qpTeQAWJP3j_VrmxIsBv3-jgkRev5kJpKrullo",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -78,7 +78,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "oDXlU6ItPg9HkwhBVntbvteFbLLQ",
     }, 
   ],
     
@@ -102,10 +102,9 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "生日", "name": "臭猪", "year": "1995", "date": "08-01"},
+    {"type": "节日", "name": "中秋", "year": "2022", "date": "09-10"},
+    {"type": "节日", "name": "国庆", "year": "2022", "date": "10-01"},
   ],
 
   /**
