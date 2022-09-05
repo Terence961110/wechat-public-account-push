2f6adde6bdade206ac8fd861d8750aef8b308c1d
Terence961110 <112816590+Terence961110@users.noreply.github.com>
2022-09-05 11:52:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "臭猪",
+      name: "李大帅哥",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到,
       id: "oDXlU6ItPg9HkwhBVntbvteFbLLQ",
       // 你想对他发送的模板消息的模板ID
@@ -37,15 +37,15 @@ export const config = {
       // 所在城市
       city: "常州",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://y.music.163.com/m/playlist?id=166723731&userid=132514428&creatorId=132514428"
+      openUrl: "https://music.163.com/#/user/songs/rank?id=132514428"
     }, 
     {
-      name: "老婆1", 
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
+      name: "臭猪", 
+      id: "oDXlU6ItPg9HkwhBVntbvteFbLLQ",
+      useTemplateId: "qW6IGLm8iIt4IJK8uiu1XTg-zfhUGQVeUACJ6d7fF8k",
+      province: "江苏",
+      city: "常州",
+      openUrl: "https://music.163.com/#/user/songs/rank?id=132514428"
     }, 
     {
       name: "老婆2",
