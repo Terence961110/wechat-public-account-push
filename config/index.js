d338951f2a407268afd433dde483bbeac98cad06
Terence961110 <112816590+Terence961110@users.noreply.github.com>
2022-10-24 16:23:06 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -104,7 +104,7 @@ export const config = {
   FESTIVALS: [
     {"type": "生日", "name": "臭猪", "year": "1995", "date": "08-01"},
     {"type": "生日", "name": "李大帅哥", "year": "1996", "date": "11-10"},
-    {"type": "节日", "name": "霜降", "year": "2022", "date": "10-23"},
+    {"type": "节日", "name": "立冬", "year": "2022", "date": "11-07"},
     {"type": "节日", "name": "万圣节前夜", "year": "2022", "date": "10-31"},
   ],
 
